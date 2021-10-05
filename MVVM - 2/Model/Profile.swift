de22c7d28038b4cae8afb3ab4644e3e917b3a66b
Roman <romaholovai@gmail.com>
2021-10-06 01:33:40 +0200

    making app in MVC arch

---
+++ MVVM - 2/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM - 2
+//
+//  Created by Roman Holovai on 06.10.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM - 2/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  MVVM - 2
+//
+//  Created by Roman Holovai on 06.10.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+}
